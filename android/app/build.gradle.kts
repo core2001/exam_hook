9f43667385178d9169310489c47da30aec57986e
core2001 <projectscorevanta@gmail.com>
2026-09-07 21:54:27 +0000

    ready for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = null
+            
         }
     }
 }
